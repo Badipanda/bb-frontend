2fc92838c372464557692a8df3d5643c8b6365c8
DESKTOP-8Q61995\Markus <mkkehrer@googlemail.com>
2019-04-23 13:44:02 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.opencast = "http://141.64.64.36/";
+config.episode_api = config.opencast + "search/episode.json";
+config.serie_api = config.opencast + "search/series.json";
+// config.apiUrl ="http://localhost:8888"
+config.apiUrl ="http://beuthbox.beuth-hochschule.de/api"
+
+module.exports = config;
